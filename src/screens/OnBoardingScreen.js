03556589dfb0c7e10bd05c5ce1adc507f9596635
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-06-03 17:22:48 +0500

    Improved OnBoarding Screen!

--- src/screens/OnBoardingScreen.js
+++ src/screens/OnBoardingScreen.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
 const OnBoarding = props => {
+    useEffect(() => {
+        setTimeout(() => props.navigation.replace('Auth'), 1);
+    }, [])
     return (
-        <View />
+        <View style={styles.screen}>
+            <Image source={require('../../assets/images/UI/logo.png')} />
+            <Image source={require('../../assets/images/onboarding/logoText.png')} style={styles.logoText} />
+        </View>
     );
 }
 
 export default OnBoarding;
 
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff'
+    },
+    logoText: {
+        marginTop: 13
+    }
 })
