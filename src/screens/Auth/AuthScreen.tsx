7feb3f20458683de2188a393f1f8d16b3bd962f8
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-06-04 17:27:51 +0500

    Improved Logo!

--- src/screens/Auth/AuthScreen.tsx
+++ src/screens/Auth/AuthScreen.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import { hP } from '../../functions/getDPFromPercent';
 import AuthTopTabNavigator from '../../navigators/AuthNavigator/AuthTopTabNavigator/AuthTopTabNavigator';
 import constants from '../../constants/constants';
+import Logo from '../../../assets/images/UI/whiteLogo.svg'
 
 const AuthScreen = (props: any) => {
     return (
         <View style={styles.screen}>
             <View style={styles.firstPortion}>
-                {/* <Image source={require('../../../assets/images/UI/logo.png')} style={{ tintColor: 'white' }} resizeMode={'contain'} /> */}
+                <Logo color={'white'} />
             </View>
             <View style={styles.lastPortion}>
                 <AuthTopTabNavigator />
