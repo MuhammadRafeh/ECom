37f95060803d3d24e7bac27620d864df906a7505
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-06-04 22:57:58 +0500

    Done with Designing Login Screen!

--- src/screens/Auth/AuthNavigatorScreens/Login/LoginScreen.tsx
+++ src/screens/Auth/AuthNavigatorScreens/Login/LoginScreen.tsx
@@ -20,7 +20,7 @@ const LoginScreen = (props: any) => {
                     <TextInput
                         value={email}
                         onChangeText={setEmail}
-                        style={{ borderBottomWidth: 1, borderBottomColor: constants.primary, opacity: 0.62, padding: 10, paddingLeft: 54, fontSize: 16, color: 'rgba(0, 0, 0, 0.70)' }}
+                        style={styles.textInput}
                     />
                     <View style={{ position: 'absolute', bottom: 14.8, left: 16 }}>
                         <EmailSvg />
@@ -30,19 +30,19 @@ const LoginScreen = (props: any) => {
                     <TextInput
                         value={password}
                         onChangeText={setPassword}
-                        style={{ borderBottomWidth: 1, borderBottomColor: constants.primary, opacity: 0.62, padding: 10, paddingLeft: 54, fontSize: 16, color: 'rgba(0, 0, 0, 0.70)' }}
+                        style={styles.textInput}
                     />
                     <View style={{ position: 'absolute', bottom: 16, left: 16 }}>
                         <PasswordSvg />
                     </View>
                 </View>
                 <TouchableOpacity style={{ marginTop: 4, marginBottom: 6 }}>
-                    <Text style={{ textAlign: 'right', marginRight: 2, color: constants.primary, fontSize: 12, fontFamily: 'Inter' }}>Forgot Password?</Text>
+                    <Text style={styles.forgotLabel}>Forgot Password?</Text>
                 </TouchableOpacity>
                 <View style={{ width: '100%', height: hP('5.5%'), marginTop: 10 }}>
                     <Button onPress={() => { alert('Button Pressed!') }} title={'Sign in'} />
                 </View>
-                <View style={{ marginVertical: 16, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                <View style={styles.signupLabel}>
                     <Text style={{ textAlign: 'center', color: '#808080', fontFamily: 'Inter' }}>Don’t have an account? </Text>
                     <TouchableOpacity>
                         <Text style={{ color: 'black', fontFamily: 'Inter' }}>
@@ -62,13 +62,13 @@ const LoginScreen = (props: any) => {
                     </TouchableOpacity>
                 </View>
                 <View style={{ marginTop: 83 }}>
-                    <Text style={{ textAlign: 'center', color: 'black', fontFamily: 'Inter', fontSize: 12, letterSpacing: 0.2 }}>
+                    <Text style={styles.info}>
                         Amet minim mollit non deserunt ullamco est sit aliqua
                         dolor do amet sint. Velit officia consequat.
                     </Text>
                 </View>
                 <TouchableOpacity style={{ marginTop: 15 }}>
-                    <Text style={{ textAlign: 'right', color: constants.primary, fontFamily: 'Inter' }}>
+                    <Text style={styles.skip}>
                         Skip
                     </Text>
                 </TouchableOpacity>
@@ -86,5 +86,39 @@ const styles = StyleSheet.create({
     },
     scrollViewStyle: {
         marginHorizontal: '8%',
+    },
+    textInput: {
+        borderBottomWidth: 1,
+        borderBottomColor: constants.primary,
+        opacity: 0.62,
+        padding: 10,
+        paddingLeft: 54,
+        fontSize: 16,
+        color: 'rgba(0, 0, 0, 0.70)'
+    },
+    forgotLabel: {
+        textAlign: 'right',
+        marginRight: 2,
+        color: constants.primary,
+        fontSize: 12,
+        fontFamily: 'Inter'
+    },
+    signupLabel: {
+        marginVertical: 16,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    info: {
+        textAlign: 'center',
+        color: 'black',
+        fontFamily: 'Inter',
+        fontSize: 12,
+        letterSpacing: 0.2
+    },
+    skip: {
+        textAlign: 'right',
+        color: constants.primary,
+        fontFamily: 'Inter'
     }
 })
