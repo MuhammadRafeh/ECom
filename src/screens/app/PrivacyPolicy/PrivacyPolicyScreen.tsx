44a75279554692f2aec0b87d08590b719a753d04
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-06-06 23:18:22 +0500

    Managed Privacy Policy Design!

--- src/screens/app/PrivacyPolicy/PrivacyPolicyScreen.tsx
+++ src/screens/app/PrivacyPolicy/PrivacyPolicyScreen.tsx
@@ -9,18 +9,18 @@ import getFontSize from '../../../functions/getFontSize';
 const PrivacyPolicyScreen = (props: any) => {
     return (
         <View style={styles.screen}>
-            <View style={{ height: hP('12%'), backgroundColor: constants.primary, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', top: -8 }}>
+            <View style={styles.firstPortion}>
                 <TouchableOpacity style={{ position: 'absolute', left: '5%' }}>
                     <MaterialIcons size={28} color={'white'} name={'keyboard-arrow-left'} />
                 </TouchableOpacity>
                 <View>
-                    <Text style={{ fontSize: getFontSize(17), fontFamily: 'Inter', color: '#fff', fontWeight: 'bold' }}>Privacy Policy</Text>
+                    <Text style={styles.title}>Privacy Policy</Text>
                 </View>
             </View>
-            <View style={{ height: hP('88%'), backgroundColor: '#fff', alignItems: 'center' }}>
-                <View style={{ height: '100%', width: '85%', backgroundColor: '#fff', borderRadius: 24, padding: 20, top: -28 }}>
+            <View style={styles.secondPortion}>
+                <View style={styles.textContainer}>
                     <ScrollView>
-                        <Text style={{ textAlign: 'justify', fontFamily: 'Inter', fontSize: 16 }}>
+                        <Text style={styles.text}>
                             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat.
                         </Text>
                     </ScrollView>
@@ -37,5 +37,37 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: constants.primary,
         paddingTop: Constants.statusBarHeight
+    },
+    firstPortion: {
+        height: hP('12%'),
+        backgroundColor: constants.primary,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: -8
+    },
+    title: {
+        fontSize: getFontSize(17),
+        fontFamily: 'Inter',
+        color: '#fff',
+        fontWeight: 'bold'
+    },
+    secondPortion: {
+        height: hP('88%'),
+        backgroundColor: '#fff',
+        alignItems: 'center'
+    },
+    textContainer: {
+        height: '100%',
+        width: '85%',
+        backgroundColor: '#fff',
+        borderRadius: 24,
+        padding: 20,
+        top: -28
+    },
+    text: {
+        textAlign: 'justify',
+        fontFamily: 'Inter',
+        fontSize: 16
     }
 })
