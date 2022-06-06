274a9bf077841a593f59da66053ba0ebc5ced5aa
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-06-06 23:01:18 +0500

    Almost Done with Designing Privacy Policy Screen!

--- src/screens/app/PrivacyPolicy/PrivacyPolicyScreen.tsx
+++ src/screens/app/PrivacyPolicy/PrivacyPolicyScreen.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import constants from '../../../constants/constants';
+import { hP } from '../../../functions/getDPFromPercent';
+import Constants from 'expo-constants';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import getFontSize from '../../../functions/getFontSize';
 
 const PrivacyPolicyScreen = (props: any) => {
     return (
-        <View>
-            
+        <View style={styles.screen}>
+            <View style={{ height: hP('12%'), backgroundColor: constants.primary, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', top: -8 }}>
+                <TouchableOpacity style={{ position: 'absolute', left: 15 }}>
+                    <MaterialIcons size={28} color={'white'} name={'keyboard-arrow-left'} />
+                </TouchableOpacity>
+                <View>
+                    <Text style={{ fontSize: getFontSize(17), fontFamily: 'Inter', color: '#fff' }}>Privacy Policy</Text>
+                </View>
+            </View>
+            <View style={{ height: hP('88%'), backgroundColor: '#fff' }}>
+
+            </View>
         </View>
     );
 }
@@ -12,5 +27,9 @@ const PrivacyPolicyScreen = (props: any) => {
 export default PrivacyPolicyScreen;
 
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1,
+        backgroundColor: constants.primary,
+        paddingTop: Constants.statusBarHeight
+    }
 })
