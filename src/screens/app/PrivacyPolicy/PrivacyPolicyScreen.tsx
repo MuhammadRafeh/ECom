e8ff41425a13f1f6085def4156e325a8fedba9a6
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-06-06 23:14:27 +0500

    Done with Privacy Policy Screen!

--- src/screens/app/PrivacyPolicy/PrivacyPolicyScreen.tsx
+++ src/screens/app/PrivacyPolicy/PrivacyPolicyScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import constants from '../../../constants/constants';
 import { hP } from '../../../functions/getDPFromPercent';
 import Constants from 'expo-constants';
@@ -10,15 +10,21 @@ const PrivacyPolicyScreen = (props: any) => {
     return (
         <View style={styles.screen}>
             <View style={{ height: hP('12%'), backgroundColor: constants.primary, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', top: -8 }}>
-                <TouchableOpacity style={{ position: 'absolute', left: 15 }}>
+                <TouchableOpacity style={{ position: 'absolute', left: '5%' }}>
                     <MaterialIcons size={28} color={'white'} name={'keyboard-arrow-left'} />
                 </TouchableOpacity>
                 <View>
-                    <Text style={{ fontSize: getFontSize(17), fontFamily: 'Inter', color: '#fff' }}>Privacy Policy</Text>
+                    <Text style={{ fontSize: getFontSize(17), fontFamily: 'Inter', color: '#fff', fontWeight: 'bold' }}>Privacy Policy</Text>
                 </View>
             </View>
-            <View style={{ height: hP('88%'), backgroundColor: '#fff' }}>
-
+            <View style={{ height: hP('88%'), backgroundColor: '#fff', alignItems: 'center' }}>
+                <View style={{ height: '100%', width: '85%', backgroundColor: '#fff', borderRadius: 24, padding: 20, top: -28 }}>
+                    <ScrollView>
+                        <Text style={{ textAlign: 'justify', fontFamily: 'Inter', fontSize: 16 }}>
+                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat.
+                        </Text>
+                    </ScrollView>
+                </View>
             </View>
         </View>
     );
