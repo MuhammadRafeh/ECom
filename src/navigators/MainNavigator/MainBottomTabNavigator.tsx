f0910c6754e04ee308c44179ac0a946048510edf
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-06-07 18:12:13 +0500

    Mostly Added All the Screens!

--- src/navigators/MainNavigator/MainBottomTabNavigator.tsx
+++ src/navigators/MainNavigator/MainBottomTabNavigator.tsx
@@ -1,14 +1,22 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import LoginScreen from '../../screens/Auth/AuthNavigatorScreens/Login/LoginScreen';
+import AddToCartScreen from '../../screens/main/AddToCart/AddToCartScreen';
+import FavoriteScreen from '../../screens/main/Favorite/FavoriteScreen';
+import HomeScreen from '../../screens/main/Home/HomeScreen';
+import MenuScreen from '../../screens/main/Menu/MenuScreen';
+import OptionsScreen from '../../screens/main/Options/OptionsScreen';
 
 const Tab = createBottomTabNavigator();
 
 function MainBottomTabNavigator() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={LoginScreen} />
-    </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Favorite" component={FavoriteScreen} />
+            <Tab.Screen name="Options" component={OptionsScreen} />
+            <Tab.Screen name="AddToCart" component={AddToCartScreen} />
+            <Tab.Screen name="Menu" component={MenuScreen} />
+        </Tab.Navigator>
+    );
 }
 
 export default MainBottomTabNavigator;
