6d12462f14ad1e5842497dab465542829958a663
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-06-07 19:44:45 +0500

    Successfully Designed Favorites Screen!

--- src/screens/main/Favorite/FavoriteScreen.tsx
+++ src/screens/main/Favorite/FavoriteScreen.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StatusBar, FlatList, StyleSheet } from 'react-native';
+import AbstractHeader from '../../../components/UI/Header';
+import AbstractButton from '../../../components/UI/Button2';
+import CartItem from '../../../components/UI/CartItem';
 
-const FavoriteScreen = () => {
+const FavoriteScreen = (props: any) => {
+    const [products, setProducts] = useState<any>([
+        { id: 1 },
+        { id: 2 },
+        { id: 3 },
+        { id: 4 },
+        { id: 5 },
+        { id: 6 },
+    ]);
     return (
-        <View />
+        <View style={styles.container}>
+            <StatusBar backgroundColor={'#FEF3E4'} barStyle={'dark-content'} />
+            {/* Header */}
+            <AbstractHeader title={'Favourite'} />
+
+            <FlatList
+                style={{ marginLeft: 20, marginRight: 20 }}
+                showsVerticalScrollIndicator={false}
+                data={products}
+                renderItem={({ item }) => <CartItem key={item.id} />}
+                keyExtractor={item => item.id.toString()}
+            />
+
+            <View style={{ margin: 20 }}>
+                <AbstractButton buttonText={'Order Now'} />
+            </View>
+        </View>
     );
-}
+};
 
 export default FavoriteScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FEF3E4',
+    },
+});
