c22cb94cf26c43476fa74db8af5d166dcccf02f3
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-06-07 15:07:48 +0500

    Added Fonts!

---
+++ src/constants/fonts.ts
@@ -0,0 +1,6 @@
+export const EXTRA_SMALL: number = 10;
+export const SMALL: number = 12;
+export const MEDIUM: number = 14;
+export const LARGE: number = 17;
+export const EXTRA_LARGE: number = 20;
+export const HEADING_SIZE: number = 25;
